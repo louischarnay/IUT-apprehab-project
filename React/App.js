b196e651e412522a39d6b98161f7d986c32fb5ad
Louis Charnay <louis.charnay@orange.fr>
2022-01-17 10:32:02 +0100

    add page accueil

--- React/App.js
+++ React/App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { render } from 'react-dom';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Categories from './modules/Categories'
+import Lessons from './modules/Lessons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Categories/>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
