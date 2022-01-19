ef9374e36ad91fc248980fc586db26617dd57712
Louis Charnay <louis.charnay@orange.fr>
2022-01-19 14:02:45 +0100

    merged from Louis

--- React/modules/ItemList.js
+++ React/modules/ItemList.js
@@ -1,58 +1,15 @@
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
 
-const DATA = [
-  {
-    id: '1',
-    title: 'Premier Item',
-    link:'ExerciseSelectionPage'
-  },
-  {
-    id: '2',
-    title: 'Second Item',
-  },
-  {
-    id: '3',
-    title: 'Troisième Item',
-  },
-  {
-    id: '4',
-    title: 'Quatrième Item',
-  },
-  {
-    id: '5',
-    title: 'Cinquième Item',
-  },
-  {
-    id: '6',
-    title: 'Sixième Item',
-  },
-  {
-    id: '7',
-    title: 'Septième Item',
-  },
-  {
-    id: '8',
-    title: 'Huitième Item',
-  },
-  {
-    id: '9',
-    title: 'Neuvième Item',
-  },
-];
-
 const Item = (item) => (
-  <View style={styles.item} onStartShouldSetResponder={() => item.nav.navigate(item.link)}>
+  <View style={styles.item}>
     <Text style={styles.title}>{item.title}</Text>
   </View>
 );
 
-
-
-
 const ItemList = (item) => {
   const renderItem = ({ item }) => (
-    <Item title={item.title} nav={navigation} link={item.link}/>
+    <Item title={item.title} link={item.link}/>
   );
   return (
     <SafeAreaView style={styles.container}>
