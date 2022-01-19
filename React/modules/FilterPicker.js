d447b0102198c1bf49de7a09607ed62954d154b8
Louis Charnay <louis.charnay@orange.fr>
2022-01-19 11:50:05 +0100

    update FilterPicker

--- React/modules/FilterPicker.js
+++ React/modules/FilterPicker.js
@@ -1,23 +1,22 @@
-import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import {Picker} from '@react-native-picker/picker';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Dimensions, Image } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 
 const FilterPicker = () => {
   const [selectedLanguage, setSelectedLanguage] = useState(true);
     return (
       <SafeAreaView style={styles.container}>
-          <Picker
-            style={styles.picker}
-            selectedValue={selectedLanguage}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedLanguage(itemValue)
-            }>
-            <Picker.Item label="Ajout récent" value="ch1"/>
-            <Picker.Item label="Catégorie préférée" value="ch2"/>
-            <Picker.Item label="Catégorie la plus regardée" value="ch3"/>
-          </Picker>
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedLanguage}
+          onValueChange={(itemValue, itemIndex) =>
+            setSelectedLanguage(itemValue)
+          }>
+          <Picker.Item label="Ajout récent" value="ch1"/>
+          <Picker.Item label="Catégorie préférée" value="ch2"/>
+          <Picker.Item label="Catégorie la plus regardée" value="ch3"/>
+        </Picker>
       </SafeAreaView>
     );
 }
@@ -30,10 +29,9 @@ const styles = StyleSheet.create({
     marginTop: 40,
     borderWidth: 3,
     borderColor: '#7D7A79',
-    borderRadius: 10
+    borderRadius: 10,
   },
   picker: {
-    borderWidth: 5,
     marginLeft: 10,
     marginRight: 10,
   },
