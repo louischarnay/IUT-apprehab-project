9f493071754c61ce48738de06d56544763f7c427
admin <remy.roussel@etu.univ-lyon1.fr>
2022-01-19 16:45:46 +0100

    Exercise Screen created and finished

--- React/App.js
+++ React/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
-<<<<<<< HEAD
-
 
 import Home from './stacks/Home';
 import Profile from './stacks/Profile';
 import Challenge from './stacks/Challenge';
 
-=======
-import Home from './stacks/Home';
-import Profile from './stacks/Profile';
-import Challenge from './stacks/Challenge';
->>>>>>> developp
 
 const Stack = createStackNavigator();
 
@@ -21,17 +14,11 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-<<<<<<< HEAD
 
           <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
           <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
           <Stack.Screen name="Challenge" component={Challenge} options={{headerShown: false}}/>
 
-=======
-          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-          <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-          <Stack.Screen name="Challenge" component={Challenge} options={{headerShown: false}}/>
->>>>>>> developp
         </Stack.Navigator>
       </NavigationContainer>
     )
