497efd86cfd442c24ee71820f00c471f487a9ba7
admin <remy.roussel@etu.univ-lyon1.fr>
2022-01-21 09:05:44 +0100

    fixing merge

--- React/stacks/Home.js
+++ React/stacks/Home.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import MainPage from '../screens/MainPage';
 import ThemesPage from '../screens/ThemesPage';
 import ExercisesPage from '../screens/ExercisesPage'
-import LessonPage from '../screens/LessonPage';
+import ExerciseScreen from '../screens/ExerciseScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +13,7 @@ const Home = () => {
         <Stack.Screen name="MainPage" component={MainPage} options={{headerShown: false}}/>
         <Stack.Screen name="ThemesPage" component={ThemesPage} options={{headerShown: false}}/>
         <Stack.Screen name="ExercisesPage" component={ExercisesPage} options={{headerShown: false}}/>
-        <Stack.Screen name="LessonPage" component={LessonPage} options={{headerShown: false}}/>
+        <Stack.Screen name="ExerciseScreen" component={ExerciseScreen} options={{headerShown:false}}/>
     </Stack.Navigator>
     )
 }
