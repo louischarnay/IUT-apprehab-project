308cbab9e2683fbe0e026416d1e50750494309bb
Louis Charnay <louis.charnay@orange.fr>
2022-01-24 18:56:16 +0100

    Update Lesson.js

--- React/modules/Lesson.js
+++ React/modules/Lesson.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import { SafeAreaView, View, StyleSheet, FlatList, Text } from 'react-native';
 
-var DATA = [];
-var cpt1 = 0;
-var cpt2 = 0;
-
-function fillDATA(params) {
-  DATA.length = 0;
-  for (cpt1 = 0; cpt1 < params.length; cpt1++){
-    for (cpt2 = 0; cpt2 < params[cpt1].content.length; cpt2++){
-      DATA[params[cpt1].content.length] = {
-        id: cpt2,
-        desc: params[cpt1].content[cpt2],
-      }
-      DATA[0] = DATA[1];
-    };
-    for (cpt2 = 1; cpt2 < params[cpt1].content.length; cpt2++){
-      DATA[cpt2-1] = DATA[cpt2];
-    };
-    DATA.length--;
-  };
-};
-
 const Item = (item) => (
   <View style={styles.itemContentExercise}>
-    <Text style={styles.desc}>{item.desc}</Text>
+    <Text style={styles.data}>{item.data}</Text>
   </View>
 );
 
 const Lesson = (params) => {
-  fillDATA(params.content)
   const renderItem =({item})=>(
-    <Item desc={item.desc}/>
+    <Item data={item.data}/>
   );
   
   return(
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
+        data={params.content[0].content}
         renderItem={renderItem}
         keyExtractor={item => item.id}
       />
@@ -49,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  desc:{
+  data: {
     marginHorizontal: 15,
     fontSize: 20,
     textAlign: 'justify',
