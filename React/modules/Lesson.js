b477fbff7101aea67107859c3e1aad9153d43dc5
admin <remy.roussel@etu.univ-lyon1.fr>
2022-01-26 14:07:20 +0100

    Adding finish exercise button

--- React/modules/Lesson.js
+++ React/modules/Lesson.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, View, StyleSheet, Dimensions,  FlatList, Image, Text } from 'react-native';
+import EndLesson from '../screens/EndLesson';
 
 function printObject(params) {
   switch (params.type){
@@ -8,6 +9,8 @@ function printObject(params) {
     case 'Image':
       const source = ({uri:'https://apprehab.000webhostapp.com/'+ params.data + '?' + new Date()});
       return <ItemImage data={source}/>
+    case 'Bouton':
+      return <ItemBouton data={params.data}/>
   }
 }
 
@@ -21,6 +24,15 @@ const ItemImage = (item) => (
   <View style={styles.itemContentExercise}>
     <Image source={item.data} style={styles.im}/>
   </View>
+)
+
+const ItemBouton =(item) =>(
+  <Pressable 
+  style={[styles.button,styles.buttonOpen]}
+  onPress={()=>setModalVisible(true)}
+  >
+  <Text style={styles.textStyle}>Finir l'exercice</Text>
+</Pressable>
 )
 
 const Lesson = (params) => {
@@ -51,7 +63,24 @@ const styles = StyleSheet.create({
   im: {
     maxWidth: Dimensions.get('window').width,
     height: Dimensions.get('window').height/3
-  }
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+
 });
   
 export default Lesson;
