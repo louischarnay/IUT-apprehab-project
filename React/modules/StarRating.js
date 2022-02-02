dfbff2dc579b5d06ace1c5cfcc01c00b8e1f6137
admin <remy.roussel@etu.univ-lyon1.fr>
2022-02-02 16:49:47 +0100

    Finishing StarRating

--- React/modules/StarRating.js
+++ React/modules/StarRating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View,StyleSheet, SafeAreaView, FlatList, Text,Image } from 'react-native'
+import { View,StyleSheet, SafeAreaView, FlatList, Text,Image,Dimensions } from 'react-native'
 
 // Use prebuilt version of RNVI in dist folder
 
@@ -7,30 +7,25 @@ const rate={};
 const RowStar=[
     {   
         id:'1',
-        icon:require('../assets/icones/star-solid.png'),
         color: "#00000"
 
     },
     {
         id:'2',
-        icon:require('../assets/icones/star-solid.png'),
         color: "#00000"
     },
     {   
         id:'3',
-        icon:require('../assets/icones/star-solid.png'),
         color: "#00000"
         
     },
     {   
         id:'4',
-        icon:require('../assets/icones/star-solid.png'),
         color: "#00000"
         
     },
     {
         id:'5',
-        icon:require('../assets/icones/star-solid.png'),
         color: "#00000"
         
     }
@@ -39,12 +34,11 @@ const RowStar=[
 
 const Item = (item) => (
     <View style={styles.item} backgroundColor={item.color} onStartShouldSetResponder={rate}>
-      <Image
-        style={styles.icon}
-        source={item.icon}  
-      />
-      <Text>{item.id}</Text>
-    </View>
+    <Image
+      style={styles.icon}
+      source={require('../assets/icones/star-solid.png')}  
+    />
+  </View>
   );
 
 
@@ -52,40 +46,41 @@ const GlobalStarRating =()=>{
     const renderItem = ({item}) => (
         <Item rate={item.id} color={item.color} icon={item.icon}/>
       );
-    return(
+    return (
         <SafeAreaView style={styles.container}>
             <FlatList
-                data={RowStar}
-                horizontal={true}
-                renderItem={renderItem}
-                 keyExtractor={item => item.id}
+                data= {RowStar}
+                horizontal ={true}
+                renderItem= {renderItem}
+                 keyExtractor= {item => item.id}
             />
         </SafeAreaView>
       );
 }
 
 
-    const styles=StyleSheet.create({
+    const styles = StyleSheet.create({
         container: {
             flex: 1,
+            borderTopRightRadius: 10,
+            borderTopLeftRadius: 10,
+            marginTop: 20,
+          },
+          item: {
+            flex: 1,
+            width: Dimensions.get('window').width/3.1,
+            height: Dimensions.get('window').height/10,
+            margin: 2,
+            marginTop: 7,
+            padding: 0,
+            borderRadius: 10,
+            justifyContent: 'flex-end',
             alignItems: 'center',
-            justifyContent: 'center',
-           // paddingTop: Constants.statusBarHeight,
-            backgroundColor: '#ecf0f1',
           },
-          tite: {
-            fontSize: 24,
-            textAlign: 'center',
-            color: '#34495e'
+          icon: {
+            width:  Dimensions.get('window').width/10,
+            height:  Dimensions.get('window').width/10
           },
-          paragraph: {
-            margin: 10,
-            marginTop: 40,
-            fontSize: 18,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            color: '#34495e',
-          }
     });
     
 export default GlobalStarRating;
