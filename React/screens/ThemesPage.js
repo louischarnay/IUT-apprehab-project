b2f605ce2081b480ab00c44a969ab281a0089a51
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2022-02-02 17:24:16 +0100

    function search works

--- React/screens/ThemesPage.js
+++ React/screens/ThemesPage.js
@@ -3,25 +3,65 @@ import {Button, StyleSheet, TextInput, View} from 'react-native';
 import ItemList from '../modules/ItemList';
 import NavigBar from '../modules/NavigBar';
 
-const ThemesPage = ({route, navigation}) => {
-  const DATA = route.params;
-  const color = route.params.color;
+class ThemesPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.data = props.route.params.DATA.DATA
+        this.color = props.route.params.color
+        this.navigation = props.navigation
+        this.searchedText = ""
+        this.state = {
+            data : props.route.params.DATA.DATA
+        }
+        console.log(this.state.data)
+    }
 
-  return (
-    <View style={{flex: 1}}>
-      <View style={styles.main_container}>
-          <TextInput style={styles.textinput}/>
-          <Button style={styles.button} color={color} title='Rechercher' onPress={() => {}}/>
-      </View>
-      <View style={{flex: 5}}>
-        <ItemList navigation={navigation} DATA={DATA.DATA.DATA} color={color}/>
-      </View>
-      <View style={{flex: 1}}>
-        <NavigBar navigation={navigation}/>
-      </View>
-    </View>
-  );
-};
+    filterThemes(data){
+        if(this.searchedText === ""){
+            return
+        }
+        let newData = Array()
+        const regex = new RegExp('[.]*' + this.searchedText + '[.]*')
+        for (let cpt = 0; cpt < data.length; cpt++){
+            if(regex.test(data[cpt]['title'])){
+                newData[newData.length] = data[cpt]
+            }
+        }
+        this.setState({
+            data : newData
+        })
+    }
+
+    updateSearchedText(text) {
+        this.searchedText = text
+    }
+
+    resetFilters() {
+        this.setState({
+            data : this.data
+        })
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <View style={styles.main_container}>
+                    <TextInput style={styles.textinput} onChangeText={(text) => {
+                        this.updateSearchedText(text)
+                    }}/>
+                    <Button style={styles.button} color={this.color} title='Rechercher' onPress={() => this.filterThemes(this.state.data)}/>
+                    <Button style={styles.button} color={this.color} title='Effacer' onPress={() => this.resetFilters()}/>
+                </View>
+                <View style={{flex: 5}}>
+                    <ItemList navigation={this.navigation} DATA={this.state.data} color={this.color}/>
+                </View>
+                <View style={{flex: 1}}>
+                    <NavigBar navigation={this.navigation}/>
+                </View>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     main_container: {
