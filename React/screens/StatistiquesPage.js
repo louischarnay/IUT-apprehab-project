ae9e036164a14fa1ee6b9f61467897ce4d4d80a0
admin <remy.roussel@etu.univ-lyon1.fr>
2022-02-03 19:48:26 +0100

    Unalerted merge errors fixed

--- React/screens/StatistiquesPage.js
+++ React/screens/StatistiquesPage.js
@@ -4,29 +4,14 @@ import CategoriePref from '../modules/CategoriePref';
 import NumActivite from '../modules/NumActivites'
 import NavigBar from '../modules/NavigBar';
 
-<<<<<<< HEAD
-=======
 
-const semaine="Cette semaine, j'ai fait ";
-const activites=" activités";
-const ItemNumActivite = ()=>(
-  <View>
-    <Text> {semaine+global.amountExerciceDoneMonth+activites}</Text>
-  </View>
-)
-
-
->>>>>>> Rémy
 const StatistiquesPage = ({navigation}) => {
     return (
       <View style={{flex: 1}}>
         <View style={{flex: 6}}>
             <CategoriePref title={global.catPref}/>
-<<<<<<< HEAD
             <NumActivite/>
-=======
-            <ItemNumActivite/>
->>>>>>> Rémy
+
         </View>
         <View style={{flex: 1}}>
           <NavigBar navigation={navigation} route={'ProfilePage'}/>
