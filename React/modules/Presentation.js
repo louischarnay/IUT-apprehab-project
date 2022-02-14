45b5ac9e9bb15a933ba8f6b3b5b32c46d239d3c4
admin <remy.roussel@etu.univ-lyon1.fr>
2022-02-14 17:44:10 +0100

    Creating Who are we ? page

---
+++ React/modules/Presentation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text} from 'react-native';
+
+
+
+const Presentation=()=>{
+    <View>
+    <Text>Qui sommes-nous ?</Text>
+
+    <Text>{}</Text>
+    </View>
+
+}
+export default Presentation
