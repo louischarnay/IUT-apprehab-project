b74c4169a67d12c278d8604337d323d30a4067d8
admin <remy.roussel@etu.univ-lyon1.fr>
2022-02-28 17:43:14 +0100

    Dark mode is a no

--- React/screens/PreferencesPage.js
+++ React/screens/PreferencesPage.js
@@ -4,12 +4,14 @@ import ClearHistorique from '../modules/ClearHistorique'
 import ControlVibrations from '../modules/ControlVibrations'
 import { View } from 'react-native';
 
+
 const PreferencesPage = ({route, navigation}) => {
   const DATA = route.params;
   return (
     <View style={{flex: 1}}>
       <View style={{flex: 6}}>
         <ClearHistorique/>
+        
       </View>
       <View style={{flex: 1}}>
         <NavigBar navigation={navigation}/>
