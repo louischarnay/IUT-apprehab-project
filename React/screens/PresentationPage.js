8dffb31cd7c789d088d77bb695fbc1c0d0dd65c4
admin <remy.roussel@etu.univ-lyon1.fr>
2022-03-05 01:05:45 +0100

    maybe finished qui sommes nous

--- React/screens/PresentationPage.js
+++ React/screens/PresentationPage.js
@@ -7,12 +7,13 @@ import ItemList from '../modules/ItemList';
 
 const PresentationPage=({route,navigation})=>{
     const DATA = route.params;
+    const TextPresentation="I'm stupid";
   const color = route.params.color;
     return(
         <View style={{flex:1}}> 
                 <View style={{flex: 5}}>
         <ItemList navigation={navigation} DATA={DATA.DATA.DATA} color={color}/>
-        <Text>I'm Stupid</Text>
+        <Text>{TextPresentation}</Text>
       </View>
               <View style={{flex:1}}>
                 <NavigBar navigation={navigation}/>
--- React/stacks/Profile.js
+++ React/stacks/Profile.js
@@ -6,6 +6,7 @@ import StatistiquesPage from '../screens/StatistiquesPage';
 import HistoriquePage from '../screens/HistoriquePage';
 import LexiquePage from '../screens/LexiquePage';
 import LessonPage from '../screens/LessonPage';
+import PresentationPage from '../screens/PresentationPage';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const Profile = () => {
     <Stack.Navigator>
         <Stack.Screen name="ProfilePage" component={ProfilePage} options={{headerShown: false}}/>
         <Stack.Screen name="PreferencesPage" component={PreferencesPage} options={{headerShown: false}}/>
+        <Stack.Screen name='PresentationPage' component={PresentationPage} options={{headerShown:false}}/>
         <Stack.Screen name="StatistiquesPage" component={StatistiquesPage} options={{headerShown: false}}/>
         <Stack.Screen name="HistoriquePage" component={HistoriquePage} options={{headerShown: false}}/>
         <Stack.Screen name="LexiquePage" component={LexiquePage} options={{headerShown: false}}/>
